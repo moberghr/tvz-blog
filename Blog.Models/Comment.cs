210b31ad030383642319cce1de147bc087d88109
Saša Kajfeš <sasa.kajfes@moberg.hr>
2022-05-10 14:14:10 +0200

    add comment list

---
+++ Blog.Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public int PostId { get; set; }
+    [MaxLength(255)]
+    public string Author { get; set; }
+    public string Text { get; set; }
+    public DateTime CreatedOn { get; set; }
+}
